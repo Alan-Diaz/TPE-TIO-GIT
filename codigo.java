62af9abd905b8ca7002aeab87050a39d6bfbd9fa
Alan <alanyd.ph@gmail.com>
2020-03-01 10:39:35 -0300

    ultimas modificaciones de codigo

--- codigo.java
+++ codigo.java
@@ -2,12 +2,10 @@ public static void main(String [] args){
     int uno;
     int dos;
     system.out.println(uno+dos);
-
-    int cinco;
-    int seis;
-
     int tres;
     int cuatro = 1;
+    int cinco;
+    int seis;
     
 
 }
